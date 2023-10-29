010d4cc446d7f665464aa5fdfbd412ec89d59ab4
Nathaniel Hudson <nchudson88@gmail.com>
2023-10-29 16:23:53 -0500

    Update subsets.py

--- flox/data/subsets.py
+++ flox/data/subsets.py
@@ -8,30 +8,3 @@ T_co = TypeVar("T_co", covariant=True)
 FederatedSubsets = NewType(
     "FederatedSubsets", dict[FlockNodeID, Union[Dataset[T_co], Subset[T_co]]]
 )
-
-# class FederatedSubsets:
-#     def __init__(self, mapping: Mapping[FlockNodeID, Dataset[T_co] | Subset[T_co]]):
-#         super().__init__()
-#         self.mapping = mapping
-#
-#     def load(self, idx: FlockNodeID) -> Dataset[T_co] | Subset[T_co]:
-#         """
-#         Get the subset of data associated with the Flock node identified by ``idx``.
-#
-#         Args:
-#             idx (FlockNodeID): The identifier of the Flock node.
-#
-#         Returns:
-#             The subset of data the identified Flock node owns.
-#         """
-#         return self.mapping[idx]
-#
-#     def __len__(self) -> int:
-#         """
-#         Gets the number of worker nodes with a subset of the data (i.e., the length of
-#         the ``mapping`` data member).
-#
-#         Returns:
-#             Number of worker nodes with subsets according to ``self.mapping``.
-#         """
-#         return len(self.mapping)
