29014dad94dbc546f9d4b63d95cbef31b3943413
Nathaniel Hudson <nchudson88@gmail.com>
2024-06-27 10:12:08 -0500

    Update types.py
    Fixed torch typing.

--- flox/learn/types.py
+++ flox/learn/types.py
@@ -1,9 +1,5 @@
-from __future__ import annotations
-
+import torch
 import typing as t
-
-if t.TYPE_CHECKING:
-    import torch
 
 Kind: t.TypeAlias = t.Literal["async", "sync", "sync-v2"]
 """..."""
@@ -11,8 +7,8 @@ Kind: t.TypeAlias = t.Literal["async", "sync", "sync-v2"]
 Where: t.TypeAlias = t.Literal["local", "globus_compute"]
 """..."""
 
-Params: t.TypeAlias = dict[str, 'torch.Tensor']  # torch.optim.Optimizer.StateDict
+Params: t.TypeAlias = dict[str, torch.Tensor]  # torch.optim.Optimizer.StateDict
 """The state dict of PyTorch ``torch.learn.Module`` (see ``torch.learn.Module.params()``)."""
 
-Loss: t.TypeAlias = 'torch.Tensor'
+Loss: t.TypeAlias = torch.Tensor
 """..."""
